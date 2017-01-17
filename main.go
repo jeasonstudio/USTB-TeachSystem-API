1eb5a74332ed5e8b5adcc9972bec8b52e04c4027
赵吉彤 <zjtong3576@sina.com>
2017-01-17 16:27:36 +0800

    beego init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "USTB-TeachSystem-API/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
